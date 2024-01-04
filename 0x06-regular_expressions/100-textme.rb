96082ece3a58bf1038442e663b76ddb9ef663ea6
awab228 <awab228@yahoo.com>
2024-01-05 01:47:32 +0200

    fix

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,8 +1,7 @@
 #!/usr/bin/env ruby
-# Author Bereket Dereje Mekonnen
 # This script output: [SENDER],[RECEIVER],[FLAGS].
 #   The sender phone number or name (including country code if present)
 #   The receiver phone number or name (including country code if present)
-#   The flags that were usedIt finds and matches htn, hbtn, hbn.
+#   The flags that were used
 
 puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
